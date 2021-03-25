bcca7a12697e30f89fd911b0d9545a6ac94b70a8
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-25 16:47:31 +0200

    Import and test `analyze_mailbox`

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import imaplib
 from config import imap_server, username, password
-from functions import get_mailboxes
+from functions import get_mailboxes, analyze_mailbox
 
 # Connect to the imap server
 imap = imaplib.IMAP4_SSL(imap_server)
@@ -8,6 +8,8 @@ imap.login(username, password)
 
 mailboxes = get_mailboxes(imap)
 print(mailboxes)
+result = analyze_mailbox(imap, "Inbox")
+print(result)
 
 # Disconnect from the imap server
 imap.logout()
