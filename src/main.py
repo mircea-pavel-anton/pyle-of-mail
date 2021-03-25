f2ac443afaa1245165d856c01d749f9beeae3fff
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-25 16:37:24 +0200

    Import `get_mailboxes` and print all mailboxes

--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
 import imaplib
 from config import imap_server, username, password
+from functions import get_mailboxes
 
 # Connect to the imap server
 imap = imaplib.IMAP4_SSL(imap_server)
 imap.login(username, password)
 
-def list_mailboxes():
-    response_code, folders = imap.list()
-    print('Available folders(mailboxes) to select:')
-    for folder_details_raw in folders:
-        folder_details = folder_details_raw.decode().split()
-        print(f'- {folder_details[-1]}')
-
-list_mailboxes()
+mailboxes = get_mailboxes(imap)
+print(mailboxes)
 
 # Disconnect from the imap server
 imap.logout()
