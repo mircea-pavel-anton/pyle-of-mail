097078b038c4ec88def6bc2cc627e42f5b985db3
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-25 16:47:17 +0200

    Add function to analyze a mailbox

--- src/functions.py
+++ src/functions.py
@@ -18,3 +18,43 @@ def get_mailboxes(imap):
 
     # return the array of available mailboxes
     return mailboxes
+
+# Returns the sender of the given message
+def extract_sender(imap, message_number):
+    # Fetch the given message based on the number
+    _, msg = imap.fetch(message_number, '(RFC822)')
+
+    # Parse the data bytes
+    mail = email.message_from_bytes(msg[0][1])
+
+    # Return the sender field
+    return mail["from"]
+
+# Analyzes the emails in the given mailbox.
+# It returns a dictionary containing the address of the sender
+# as the key, and the number of emails received from them as the
+# value.
+def analyze_mailbox(imap, mailbox):
+    # Select the given mailbox as to only analyze it
+    imap.select(mailbox)
+
+    # A dictionary that will contain pairs in this format:
+    # { "sender_address@provider.whatever": number_of_emails_received_from_them }
+    senders = {}
+
+    # Fetch all message numbers in the given mailbox
+    _, message_numbers_raw = imap.search(None, 'ALL')
+
+    # Loop through all available emails
+    for message_number in message_numbers_raw[0].split():
+        # Extract the email sender
+        sender = extract_sender(imap, message_number)
+
+        # If the doesn't exist in the dictionary, add it with a value of 1
+        # Otherwise, increment the associated number
+        if sender not in senders:
+            senders[sender] = 1
+        else:
+            senders[sender] += 1
+
+    return senders
