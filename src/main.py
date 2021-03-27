7f7ef78a853e4f7463c4745a748f33b545c2691a
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-27 22:33:28 +0200

    Add some logging and import all required vars from config

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
-from config import imap_server, username, password
-from functions import analyze_mailbox, filter_mailbox
+from config import imap_server, username, password, mailboxes, sleep_time
+from functions import filter_mailbox, log
+
 from imap_tools import MailBox
 from time import sleep
 
 # Connect to the imap server
+log('Logging in to ' + imap_server + '...')
 imap = MailBox(imap_server).login(username, password)
+log('OK')
 
 # Filtering is done here...
 while True:
     for mailbox in mailboxes:
+        log('Filtering started on' + mailbox)
         filter_mailbox(imap, mailbox)
+    log('Napsiees!')
     sleep(sleep_time)
+    log('Gluten morgan!')
 
 # Disconnect from the imap server
 imap.logout()
