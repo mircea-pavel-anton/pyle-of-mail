8aaa923fed8c8dfbdd82f1791fffa4e7e7128479
Mircea Anton <mircea.anton99@yahoo.com>
2021-03-28 01:19:15 +0200

    Restructure infinite loop + use imap connect/disconnect functions

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
-from config import imap_server, username, password, mailboxes, sleep_time
-from functions import filter_mailbox, log
+from config import mailboxes, sleep_time
+from functions import filter_mailbox, log, create_folders, imap_connect, imap_disconnect
 
 from imap_tools import MailBox
 from time import sleep
 
-# Connect to the imap server
-log('Logging in to ' + imap_server + '...')
-imap = MailBox(imap_server).login(username, password)
-log('OK')
 
-# Filtering is done here...
+# Connect to the imap server, ensure the needed
+# mailboxes are present and then disconnect
+imap = imap_connect()
+create_folders(imap)
+imap_disconnect(imap)
+
+# Loop indefinetly
 while True:
+    # Connect to the imap server using the credentials found in config.py
+    imap = imap_connect()
+
+    # Loop through all the required mailboxes and filter them
     for mailbox in mailboxes:
-        log('Filtering started on' + mailbox)
+        log('Filtering started on ' + mailbox)
         filter_mailbox(imap, mailbox)
+
+    # Disconnect from the imap server
+    imap_disconnect(imap)
+
+    # Sleep for as many seconds as indicated in the config.py file
     log('Napsiees!')
     sleep(sleep_time)
     log('Gluten morgan!')
-
-# Disconnect from the imap server
-imap.logout()
